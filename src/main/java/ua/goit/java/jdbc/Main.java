7a798f8f3d46dd1db8cebabc85e346b55d02dc52
AlexandrTheMacedonian <Sanyasemenov96@gmail.com>
2017-02-28 17:37:24 +0200

    jdbc homework

--- src/main/java/ua/goit/java/jdbc/Main.java
+++ src/main/java/ua/goit/java/jdbc/Main.java
@@ -27,10 +27,13 @@ public class Main {
     }
 
     private void start() throws SQLException {
+
         controller.createProject(15, "Project1", 5000, new ArrayList<>());
         controller.createDeveloper(15, "Alex", 38088888, 7500, new ArrayList<>());
+
         System.out.println(controller.getDeveloperByID(1));
         System.out.println(controller.getProjectByID(1));
+
         controller.deleteDeveloper(15);
         controller.deleteProject(15);
     }
